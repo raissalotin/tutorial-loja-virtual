e6ec63986eaa55e1ae051a83168deecb0243e3dd
raissalotin <rahlotin@gmail.com>
2024-07-26 13:14:07 -0300

    inclusao de dados

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hq462pq1l2g"
+    "revision": "0.l0lbrhn84dg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
